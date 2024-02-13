fd4e9be53ec6c0de03e747c6e773f608b3a6c3fe
John Morton <john.morton@institutedata.com>
2024-02-13 21:07:52 +0800

    add context functions

--- src/features/UserManager/UserContext.jsx
+++ src/features/UserManager/UserContext.jsx
@@ -5,7 +5,7 @@ const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(userReducer, initialState);
-  
+
   const value = { state, dispatch };
   return (
     <UserContext.Provider value={{ value }}>{children}</UserContext.Provider>
@@ -20,5 +20,52 @@ const useUser = () => {
   return context;
 };
 
+const getUsers = async (dispatch) => {
+  dispatch({ type: "FETCH_USERS_REQUEST" });
+  try {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+    console.log(data);
+    switch (data.result) {
+      case 200:
+        dispatch({ type: "FETCH_USERS_SUCCESS", payload: data.data });
+        break;
+      case 404:
+        dispatch({ type: "FETCH_USERS_FAILURE", payload: "Endpoint not found" });
+        break;
+      case 500:
+        dispatch({ type: "FETCH_USERS_FAILURE", payload: data.message });
+        break;
+      default:
+        dispatch({ type: "FETCH_USERS_FAILURE", payload: "Something went wrong. Please try again later." });
+        break;
+    }
+  } catch (error) {
+    console.error("Error fetching users", error);
+    dispatch({ type: "FETCH_USERS_FAILURE", payload: "Something went wrong. Please try again later." });
+  }
+};
+
+const deleteUser = async (dispatch, id) => {
+  const deleteUser = confirm("Are you sure you want to delete this user?");
+  if (!deleteUser) {
+    return false;
+  }
+  dispatch({ type: "DELETE_USER_REQUEST" });
+  try {
+    const response = await fetch(`http://localhost:3000/api/users/${id}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    console.log(data);
+    if (data.result === 200) {
+      dispatch({ type: "DELETE_USER_SUCCESS", payload: id });
+    }
+  } catch (error) {
+    console.error("Error deleting user", error);
+    dispatch({ type: "DELETE_USER_FAILURE", payload: "Something went wrong. Please try again later." });
+  }
+}
+
 // eslint-disable-next-line react-refresh/only-export-components
-export { UserProvider, useUser }
+export { UserProvider, useUser, getUsers, deleteUser }
--- src/features/UserManager/UserManager2.jsx
+++ src/features/UserManager/UserManager2.jsx
@@ -1,86 +1,39 @@
-// import { useEffect } from "react";
-// import List from "@mui/material/List";
-// import ListItemButton from "@mui/material/ListItemButton";
-// import ListItemText from "@mui/material/ListItemText";
-// import IconButton from "@mui/material/IconButton";
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import ListItemSecondaryAction from "@mui/material/ListItemSecondaryAction";
-// import Grid from '@mui/material/Grid';
-// import Alert from "@mui/material/Alert";
-// import Loader from "../../components/Loader";
-import { useUser } from "./UserContext";
+import { useEffect } from "react";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ListItemSecondaryAction from "@mui/material/ListItemSecondaryAction";
+import Grid from "@mui/material/Grid";
+import Alert from "@mui/material/Alert";
+import Loader from "../../components/Loader";
+import { useUser, getUsers, deleteUser } from "./UserContext";
 
 const UserManager = () => {
-    const state = useUser().state;
-    console.log(state);
-   // const dispatch = useUser().dispatch;
+  const {
+    state: { users, selectedUser, loading, error },
+    dispatch,
+  } = useUser().value;
 
-  /* useEffect(() => {
-    dispatch({ type: "FETCH_USERS_REQUEST" });
-    fetch("http://localhost:3000/api/users")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        switch (data.result) {
-          case 200:
-            dispatch({ type: "FETCH_USERS_SUCCESS", payload: data.data });
-            break;
-          case 404:
-            dispatch({ type: "FETCH_USERS_FAILURE", payload: "Endpoint not found" });
-            break;
-          case 500:
-            dispatch({ type: "FETCH_USERS_FAILURE", payload: data.message });
-            break;
-          default:
-            dispatch({ type: "FETCH_USERS_FAILURE", payload: "Something went wrong. Please try again later." });
-            break;
-        }
-        // TODO: handle all the possible responses
-      }).catch((error) => {
-        console.error("Error fetching users", error);
-        dispatch({ type: "FETCH_USERS_FAILURE", payload: "Something went wrong. Please try again later." });
-      }).finally(() => {
-        //;
-      }
-    );
-  }, []); */
+  useEffect(() => {
+    getUsers(dispatch);
+  }, [dispatch]);
 
-  /* const UserList = () => {
+  const UserList = () => {
     const handleListItemClick = (event) => {
       event.preventDefault();
       const id = event.currentTarget.dataset.id;
       dispatch({ type: "SELECT_USER", payload: id });
-    }; */
+    };
 
-    /* const deleteUser = (event) => {
-      const deleteUser = confirm("Are you sure you want to delete this user?");
-      if (!deleteUser) {
-        return false;
-      }
-      dispatch({ type: "DELETE_USER_REQUEST" });
+    const deleteUser = (event) => {
       const id = event.currentTarget.dataset.id;
-      fetch(`http://localhost:3000/api/users/${id}`, {
-        method: "DELETE",
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          if (data.result === 200) {
-            dispatch({ type: "DELETE_USER_SUCCESS", payload: id });
-          }
-        }).catch((error) => {
-          console.error("Error deleting user", error);
-          dispatch({ type: "DELETE_USER_FAILURE", payload: "Something went wrong. Please try again later." });
-        }).finally(() => {
-          // setLoading(false);
-        }
-      );
-      }; */
+      deleteUser(dispatch, id);
+    };
 
-
-
-    /* return (
-     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+    return (
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
         {users.map((user) => (
           <ListItemButton
             key={user.id}
@@ -90,25 +43,29 @@ const UserManager = () => {
           >
             <ListItemText primary={user.name} secondary={user.email} />
             <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="delete" data-id={user.id} onClick={deleteUser}>
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                data-id={user.id}
+                onClick={deleteUser}
+              >
                 <DeleteIcon />
               </IconButton>
-          </ListItemSecondaryAction>
+            </ListItemSecondaryAction>
           </ListItemButton>
         ))}
-      </List> 
-    ); 
-  };*/
+      </List>
+    );
+  };
 
   return (
-    /* <>
-      <Grid item xs={12} sm={6}> 
+    <>
+      <Grid item xs={12} sm={6}>
         {loading ? <Loader /> : <UserList />}
         {error && <Alert severity="error">This is an error Alert.</Alert>}
       </Grid>
       <Grid item xs={12} sm={6}></Grid>
-    </> */
-    <div>hi</div>
+    </>
   );
 };
 
